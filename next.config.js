fe1bcb02f1cfe1d187eafdcc8bc7d50cdd1d8421
razaogz <danial.dan8@outlook.com>
2026-09-06 14:49:58 +0530

    Fix Vercel build configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  outputFileTracingRoot: path.resolve(__dirname),
 };
 
 module.exports = nextConfig;
